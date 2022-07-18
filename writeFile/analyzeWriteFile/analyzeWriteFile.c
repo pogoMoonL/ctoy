968d13d690fc8f95a0357c45c0e1e0f07a26f257
yaodehuang <sef1548@gmail.com>
2022-07-18 18:44:21 +0000

    feat: add different struct

--- writeFile/analyzeWriteFile/analyzeWriteFile.c
+++ writeFile/analyzeWriteFile/analyzeWriteFile.c
@@ -3,8 +3,32 @@
 #include <string.h>
 #include <inttypes.h>
 
+
+struct instr_t {
+    union {
+        uint32_t imm22 : 22;
+
+        struct {
+            union {
+                uint32_t imm17: 17;
+                struct {
+                    uint8_t shamt: 5;
+                    uint8_t mode : 2;
+                    uint16_t imm10 : 10;
+                };
+            };
+            uint8_t rb : 5;
+        };
+    };
+
+    uint8_t ra : 5;
+    uint8_t op : 5;
+};
+
+
 struct Student {
-   uint16_t roll_no : 3;
+   int  :0 ;
+   int  roll_no: 3; 
    char name[20];
 };
 int main () {
@@ -14,10 +38,13 @@ int main () {
       fprintf(stderr, "\nError to open the file\n");
       exit (1);
    }
-   struct Student inp1 = { .roll_no = 7, .name = "Ram"};
-   struct Student inp2 = {2, "Shyam"};
+   
+    struct Student inp1 = {};
+    inp1.roll_no = 7;
+    strcpy(inp1.name, "Ram");
+
+    //struct Student inp1 = { .roll_no = 7, .name = "Ram"};
    fwrite (&inp1, sizeof(struct Student), 1, of);
-   fwrite (&inp2, sizeof(struct Student), 1, of);
    if(fwrite != 0)
       printf("Contents to file written successfully !\n");
    else
