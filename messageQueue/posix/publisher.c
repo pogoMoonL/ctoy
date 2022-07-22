65f2b10ebf97d4c5c5301499f097c16942b16d03
yaodehuang <sef1548@gmail.com>
2022-07-22 15:45:03 +0000

    feat: add posix message queue

---
+++ messageQueue/posix/publisher.c
@@ -0,0 +1,47 @@
+/*
+ * Example used in following article:
+ *
+ * Implementace front zpráv podle normy POSIX
+ * https://www.root.cz/clanky/implementace-front-zprav-podle-normy-posix/
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <mqueue.h>
+
+#define QUEUE_NAME "/queue1"
+
+int main(void)
+{
+    mqd_t message_queue_id;
+    unsigned int priority = 0;
+    char message_text[100];
+    int status;
+/*
+ *  O_CREAT
+ * */
+    message_queue_id = mq_open(QUEUE_NAME, O_RDWR | O_EXCL | O_CREAT  | O_CREAT | O_EXCL, S_IRWXU | S_IRWXG, NULL);
+    if (message_queue_id == -1) {
+	 if(errno == EEXIST){
+         message_queue_id = mq_open(QUEUE_NAME, O_RDWR | O_EXCL | O_EXCL, S_IRWXU |S_IRWXG, NULL);
+	 }
+       /* perror("Unable to create queue");
+        return 2;*/
+    }
+
+    strcpy(message_text, "Hello world!");
+
+    status = mq_send(message_queue_id, message_text, strlen(message_text)+1, priority);
+    if (status == -1) {
+        perror("Unable to send message");
+        return 2;
+    }
+
+    status = mq_close(message_queue_id);
+    if (status == -1) {
+        perror("Unable to close message queue");
+        return 2;
+    }
+     return 0;
+}
---
+++ messageQueue/posix/subscriber.c
@@ -0,0 +1,44 @@
+/*
+ * Example used in following article:
+ *
+ * Implementace front zpráv podle normy POSIX
+ * https://www.root.cz/clanky/implementace-front-zprav-podle-normy-posix/
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <mqueue.h>
+
+#define QUEUE_NAME "/queue1"
+
+int main(void)
+{
+    mqd_t message_queue_id;
+    char message_text[10000];
+    unsigned int sender;
+    int status;
+
+    message_queue_id = mq_open(QUEUE_NAME, O_RDWR);
+    if (message_queue_id == -1) {
+        perror("Unable to open queue");
+        return 2;
+    }
+
+    status = mq_receive(message_queue_id, message_text, sizeof(message_text), &sender);
+    if (status == -1) {
+        perror("Unable to receive message");
+        return 2;
+    }
+    printf("Received message (%d bytes) from %d: %s\n", status, sender, message_text);
+
+    status = mq_close(message_queue_id);
+    if (status == -1) {
+        perror("Unable to close message queue");
+        return 2;
+    }
+   /* mq_unlink(QUEUE_NAME); 
+    * */
+
+    return 0;
+}
